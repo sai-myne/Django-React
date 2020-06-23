df91ce300d8ea64960f64e3af1a76863eacb2239
sai-myne <ahlusir@gmail.com>
2020-06-23 15:53:44 +0800

    40 - Permissions and Authentication Classes Decorators for DRF APIs

--- tweetme2_2/tweets/views.py
+++ tweetme2_2/tweets/views.py
@@ -3,7 +3,9 @@ from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
 from django.utils.http import is_safe_url
 
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import render, redirect
 from rest_framework.response import Response
 from .forms import TweetForm
@@ -17,6 +19,8 @@ def home_view(request, *args, **kwargs):
 
 
 @api_view(['POST'])# http method the client = POST
+#@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated]) # REST API course
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
