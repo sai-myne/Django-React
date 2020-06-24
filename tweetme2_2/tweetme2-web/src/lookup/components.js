aeb9a7390650b54b96b5599c7980518f0c8dbc45
sai-myne <ahlusir@gmail.com>
2020-06-24 14:36:52 +0800

    58 - Using JavaScript Modules

---
+++ tweetme2_2/tweetme2-web/src/lookup/components.js
@@ -0,0 +1,15 @@
+export function loadTweets(callback) {
+    const xhr = new XMLHttpRequest(); // xhr = SomeClass()
+    const method = "GET"; // "POST"
+    const url = "http://localhost:8000/api/tweets";
+    const responseType = "json";
+    xhr.responseType = responseType;
+    xhr.open(method, url);
+    xhr.onload = function () {
+      callback(xhr.response, xhr.status);
+    };
+    xhr.onerror = function () {
+      callback({ message: "The request was an error" }, 400);
+    };
+    xhr.send();
+  }
---
+++ tweetme2_2/tweetme2-web/src/tweets/components.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect} from "react";
+
+import {loadTweets} from '../lookup'
+
+export function TweetsList(props) {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    // do my lookup
+    const myCallback = (response, status) => {
+      if (status === 200) {
+        setTweets(response);
+      } else {
+        alert("There was an error");
+      }
+      setTweets(response);
+    };
+    loadTweets(myCallback);
+  }, []);
+  return tweets.map((item, index) => {
+    return (
+      <Tweet
+        tweet={item}
+        key={`${index}-{item.id}`}
+        className="my-5 py-5 border bg-white text-dark"
+      />
+    );
+  });
+}
+
+
+export function ActionBtn(props) {
+  const { tweet, action } = props;
+  const className = props.className
+    ? props.className
+    : "btn btn-primary btn-sm";
+  return action.type === "like" ? (
+    <button className={className}>{tweet.likes} Likes</button>
+  ) : null;
+}
+
+export function Tweet(props) {
+  const { tweet } = props;
+  const className = props.className
+    ? props.className
+    : "col-10 mx-auto col-md-6";
+  return (
+    <div className={className}>
+      <p>
+        {tweet.id} - {tweet.content}
+      </p>
+      <div className="btn btn-group">
+        <ActionBtn tweet={tweet} action={{ type: "likes" }} />
+        <ActionBtn tweet={tweet} action={{ type: "unlikes" }} />
+      </div>
+    </div>
+  );
+}
