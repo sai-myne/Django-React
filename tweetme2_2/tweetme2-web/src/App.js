bfacbbca8b1f45185c24e83937b722c2425b2f9b
sai-myne <ahlusir@gmail.com>
2020-06-24 14:23:32 +0800

    57 - Reactjs Action Btn

--- tweetme2_2/tweetme2-web/src/App.js
+++ tweetme2_2/tweetme2-web/src/App.js
@@ -1,49 +1,66 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function loadTweets(callback) {
-  const xhr = new XMLHttpRequest() // xhr = SomeClass()
-  const method = 'GET' // "POST"
-  const url = 'http://localhost:8000/api/tweets'
-  const responseType = 'json'
-  xhr.responseType = responseType
-  xhr.open(method, url)
-  xhr.onload = function(){
-      callback(xhr.response, xhr.status)
-  }
-  xhr.onerror = function(){
-    callback({"message": "The request was an error"}, 400)
-  }
-  xhr.send()
+  const xhr = new XMLHttpRequest(); // xhr = SomeClass()
+  const method = "GET"; // "POST"
+  const url = "http://localhost:8000/api/tweets";
+  const responseType = "json";
+  xhr.responseType = responseType;
+  xhr.open(method, url);
+  xhr.onload = function () {
+    callback(xhr.response, xhr.status);
+  };
+  xhr.onerror = function () {
+    callback({ message: "The request was an error" }, 400);
+  };
+  xhr.send();
 }
 
-function Tweet(props){
-  const {tweet} = props
-  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-  return <div className={className}>
-    <p>{tweet.id} - {tweet.content}</p>
-  </div>
+function ActionBtn(props) {
+  const { tweet, action } = props;
+  const className = props.className
+    ? props.className
+    : "btn btn-primary btn-sm";
+  return action.type === "like" ? (
+    <button className={className}>{tweet.likes} Likes</button>
+  ) : null;
+}
+
+function Tweet(props) {
+  const { tweet } = props;
+  const className = props.className
+    ? props.className
+    : "col-10 mx-auto col-md-6";
+  return (
+    <div className={className}>
+      <p>
+        {tweet.id} - {tweet.content}
+      </p>
+      <div className="btn btn-group">
+        <ActionBtn tweet={tweet} action={{ type: "likes" }} />
+        <ActionBtn tweet={tweet} action={{ type: "unlikes" }} />
+      </div>
+    </div>
+  );
 }
 
 function App() {
-  const [tweets, setTweets] = useState([])
-  
+  const [tweets, setTweets] = useState([]);
 
   useEffect(() => {
     // do my lookup
     const myCallback = (response, status) => {
-      console.log(response, status)
-      if (status === 200){
-        setTweets(response)
+      if (status === 200) {
+        setTweets(response);
       } else {
-        alert("There was an error")
+        alert("There was an error");
       }
-      setTweets(response)
-    }
-    loadTweets(myCallback)    
-  }, [])
+      setTweets(response);
+    };
+    loadTweets(myCallback);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -52,8 +69,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          {tweets.map((item, index)=>{
-            return <Tweet tweet={item} key={`${index}-{item.id}`} className='my-5 py-5 border bg-white text-dark'/>
+          {tweets.map((item, index) => {
+            return (
+              <Tweet
+                tweet={item}
+                key={`${index}-{item.id}`}
+                className="my-5 py-5 border bg-white text-dark"
+              />
+            );
           })}
         </div>
         <a
