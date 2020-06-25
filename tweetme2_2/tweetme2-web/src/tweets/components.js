aa53ae1f44f9999ddbbed9505819b7b122ad25e0
sai-myne <ahlusir@gmail.com>
2020-06-25 14:23:58 +0800

    72 - Rendering the ReTweet

--- tweetme2_2/tweetme2-web/src/tweets/components.js
+++ tweetme2_2/tweetme2-web/src/tweets/components.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react";
 
-import { apiTweetAction,apiTweetCreate,apiTweetList } from "./lookup";
+import { apiTweetAction, apiTweetCreate, apiTweetList } from "./lookup";
 
 export function TweetsComponent(props) {
   const textAreaRef = React.createRef();
-  const [newTweets, setNewTweet] = useState([])
+  const [newTweets, setNewTweet] = useState([]);
   const handleBakcendUpdate = (response, status) => {
     // backend api response handler
-    let tempNewTweets = [...newTweets]
-    if(status===201){
-      tempNewTweets.unshift(response)
-      setNewTweet(tempNewTweets)
+    let tempNewTweets = [...newTweets];
+    if (status === 201) {
+      tempNewTweets.unshift(response);
+      setNewTweet(tempNewTweets);
     } else {
-      alert('An error occured, please try again')
+      alert("An error occured, please try again");
     }
-  }
+  };
   const hanldeSubmit = (event) => {
     // backend api request
     event.preventDefault();
-    const newVal = textAreaRef.current.value;    
-    apiTweetCreate(newVal, handleBakcendUpdate)    
+    const newVal = textAreaRef.current.value;
+    apiTweetCreate(newVal, handleBakcendUpdate);
     textAreaRef.current.value = "";
   };
   return (
@@ -44,26 +44,26 @@ export function TweetsComponent(props) {
 
 export function TweetsList(props) {
   const [tweetsInit, setTweetsInit] = useState([]);
-  const [tweets, setTweets] = useState([])
-  const [tweetsDidSet, setTweetsDidSet] = useState(false)
+  const [tweets, setTweets] = useState([]);
+  const [tweetsDidSet, setTweetsDidSet] = useState(false);
   useEffect(() => {
-      const final = [...props.newTweets].concat(tweetsInit)
-      if (final.length !== tweets.length){
-          setTweets(final)
-      }
-  }, [props.newTweets, tweets, tweetsInit])
+    const final = [...props.newTweets].concat(tweetsInit);
+    if (final.length !== tweets.length) {
+      setTweets(final);
+    }
+  }, [props.newTweets, tweets, tweetsInit]);
   useEffect(() => {
-    if(tweetsDidSet === false){
+    if (tweetsDidSet === false) {
       const handleTweetListLookup = (response, status) => {
         if (status === 200) {
           setTweetsInit(response);
-          setTweetsDidSet(true)
+          setTweetsDidSet(true);
         } else {
           alert("There was an error");
         }
       };
       apiTweetList(handleTweetListLookup);
-    }    
+    }
   }, [tweetsInit, tweetsDidSet, setTweetsDidSet]);
   return tweets.map((item, index) => {
     return (
@@ -87,16 +87,15 @@ export function ActionBtn(props) {
     : "btn btn-primary btn-sm";
   const actionDisplay = action.display ? action.display : "Action";
   const handleActionBackendEvent = (response, status) => {
-    console.log(response, status)
-    if(status === 200){
-      setLikes(response.likes)
+    console.log(response, status);
+    if (status === 200) {
+      setLikes(response.likes);
       //setUserLike(true)
     }
-  }
+  };
   const handleClick = (event) => {
     event.preventDefault();
-    apiTweetAction(tweet.id, action.type, handleActionBackendEvent)
-    
+    apiTweetAction(tweet.id, action.type, handleActionBackendEvent);
   };
   const display =
     action.type === "like" ? `${likes} ${actionDisplay}` : actionDisplay;
@@ -107,6 +106,16 @@ export function ActionBtn(props) {
   );
 }
 
+export function ParentTweet(props){
+  const {tweet} = props
+  return tweet.parent ? <div className="row">
+    <div className="col-11 mx-auto p-3 border rounded">
+        <p className="mb-0 text-muted small">Retweet</p>
+        <Tweet className={" "} tweet={tweet.parent} />
+      </div>
+    </div> : null
+}
+
 export function Tweet(props) {
   const { tweet } = props;
   const className = props.className
@@ -114,16 +123,22 @@ export function Tweet(props) {
     : "col-10 mx-auto col-md-6";
   return (
     <div className={className}>
-      <p>
-        {tweet.id} - {tweet.content}
-      </p>
+      <div>
+        <p>
+          {tweet.id} - {tweet.content}
+        </p>
+        <ParentTweet tweet={tweet} />
+      </div>
       <div className="btn btn-group">
         <ActionBtn tweet={tweet} action={{ type: "like", display: "Likes" }} />
         <ActionBtn
           tweet={tweet}
           action={{ type: "unlike", display: "Unlike" }}
         />
-        <ActionBtn tweet={tweet} action={{ type: "retweet", display: "Retweet" }} />
+        <ActionBtn
+          tweet={tweet}
+          action={{ type: "retweet", display: "Retweet" }}
+        />
       </div>
     </div>
   );
