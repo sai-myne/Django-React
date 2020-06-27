dc059de4e67735083f5cafecdc517de73ec65382
sai-myne <ahlusir@gmail.com>
2020-06-27 16:27:15 +0800

    106 - The Follow Button

--- tweetme2_2/tweetme2-web/src/profiles/lookup.js
+++ tweetme2_2/tweetme2-web/src/profiles/lookup.js
@@ -3,3 +3,13 @@ import {backendLookup} from '../lookup'
 export function apiProfileDetail(username, callback) {
     backendLookup("GET", `/profiles/${username}/`, callback);
 }
+
+export function apiTweetAction(tweetId, action, callback) {
+    const data = { id: tweetId, action: action }
+    backendLookup("POST", "/tweets/action/", callback, data);
+}
+
+export function apiProfileFollowToggle(username, action, callback) {
+    const data = {action: `${action && action}`.toLowerCase() }
+    backendLookup("POST", `/profiles/${username}/follow`, callback, data);
+}
