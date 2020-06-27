d32570ad9bc34376184b4078caf19de57ffef2e3
sai-myne <ahlusir@gmail.com>
2020-06-27 15:24:44 +0800

    103 - User Profile API Detail

--- tweetme2_2/profiles/api/urls.py
+++ tweetme2_2/profiles/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
     user_follow_view, 
+    profile_detail_api_view,
     )
 '''
 CLIENT
@@ -9,5 +10,6 @@ Base ENDPOINT /api/profiles/
 '''
 
 urlpatterns = [
-    path('<str:username>/follow/', user_follow_view),
+    path('<str:username>/', profile_detail_api_view),
+    path('<str:username>/follow', user_follow_view),
 ]
