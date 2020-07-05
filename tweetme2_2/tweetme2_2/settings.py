2d105274287d44750e45b608e8604a54ae2ba3e3
sai-myne <52918521+sai-myne@users.noreply.github.com>
2020-07-05 10:14:17 +0800

    Update settings.py

--- tweetme2_2/tweetme2_2/settings.py
+++ tweetme2_2/tweetme2_2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$67e1khe)pucr+k$s4!a(+sfxk1s()2$g=w40+ak2$hoo7k9o^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
